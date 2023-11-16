ea555b72cd2c456819ed70269f53742d7d1095b5
antoniogithub13 <45179139+antoniogithub13@users.noreply.github.com>
2023-11-16 19:31:44 +0100

    Update tfrecord_file.py

--- wormpose/machine_learning/tfrecord_file.py
+++ wormpose/machine_learning/tfrecord_file.py
@@ -83,7 +83,7 @@ class Writer(object):
             "label0": tf.train.Feature(float_list=tf.train.FloatList(value=label_0.tolist())),
             "label1": tf.train.Feature(float_list=tf.train.FloatList(value=label_1.tolist())),
             "length": tf.train.Feature(float_list=tf.train.FloatList(value=[length])),
-            "skel": tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_data.tostring()])),
+            "skel": tf.train.Feature(bytes_list=tf.train.BytesList(value=[skel.tostring()])),
         }
         example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
         self.record_writer.write(example_proto.SerializeToString())
